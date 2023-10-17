582b53572a7acbb50d912ab85a6ee192c3f4598c
Maksim Tisheninov <41760206+MaxLost@users.noreply.github.com>
2023-10-17 23:49:09 +0300

    Added base structure of a project

---
+++ src/test/java/edu/spbu/datacontrol/CommunityDataControlApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.spbu.datacontrol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityDataControlApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
