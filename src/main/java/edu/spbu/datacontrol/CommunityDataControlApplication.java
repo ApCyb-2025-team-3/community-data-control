28a9ef38001d2670be50ab0bbba56280ae45a81a
Maksim Tisheninov <tisheninov.maxim@gmail.com>
2023-10-17 23:03:17 +0300

    Initial commit

---
+++ src/main/java/edu/spbu/datacontrol/CommunityDataControlApplication.java
@@ -0,0 +1,13 @@
+package edu.spbu.datacontrol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommunityDataControlApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(CommunityDataControlApplication.class, args);
+  }
+
+}
