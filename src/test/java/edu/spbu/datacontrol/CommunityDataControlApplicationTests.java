8ba68c49eb41a5833d783336ecddda9db38391af
Maksim Tisheninov <tisheninov.maxim@gmail.com>
2023-10-19 12:39:44 +0300

    WIP

--- src/test/java/edu/spbu/datacontrol/CommunityDataControlApplicationTests.java
+++ src/test/java/edu/spbu/datacontrol/CommunityDataControlApplicationTests.java
@@ -1,13 +1,20 @@
 package edu.spbu.datacontrol;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = Replace.NONE)
 class CommunityDataControlApplicationTests {
-
   @Test
   void contextLoads() {
+    assertTrue(true);
   }
 
 }
