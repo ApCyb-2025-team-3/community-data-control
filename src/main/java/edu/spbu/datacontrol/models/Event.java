e3d788ee9d89e648171f0dd76f6683355b52ef2c
Maksim Tisheninov <tisheninov.maxim@gmail.com>
2023-10-20 00:37:39 +0300

    Data models updated

--- src/main/java/edu/spbu/datacontrol/models/Event.java
+++ src/main/java/edu/spbu/datacontrol/models/Event.java
@@ -3,18 +3,30 @@ package edu.spbu.datacontrol.models;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
 import java.util.Date;
 import lombok.Data;
 import jakarta.persistence.Id;
+import org.springframework.data.annotation.CreatedDate;
 
 @Entity
 @Data
+@Table(name = "event_log")
 public class Event {
 
   @Id
   @GeneratedValue(strategy = GenerationType.AUTO)
   long id;
 
+  String type;
+
+  @CreatedDate
   Date createdAt;
+
+  String previousValue;
+
+  String newValue;
+
+  String description;
 
 }
--- src/main/java/edu/spbu/datacontrol/models/User.java
+++ src/main/java/edu/spbu/datacontrol/models/User.java
@@ -5,7 +5,9 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import java.util.Date;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
 
 @Entity
 @Data
@@ -16,6 +18,37 @@ public class User {
   @GeneratedValue(strategy = GenerationType.AUTO)
   long id;
 
+  @CreatedDate
+  Date invitedAt;
+
+  boolean isActive;
+
   String name;
 
+  Date dob;
+
+  String email;
+
+  String phoneNumber;
+
+  // TODO: Maybe change to ID if supervisor is in system
+  String supervisorName;
+
+  long teamleadId;
+
+  long[] productOwnersIds;
+
+  // TODO: Maybe change to group ID
+  String project;
+
+  String department;
+
+  // TODO: Change to enum
+  String grade;
+
+  // TODO: Change to enum
+  String role;
+
+  // TODO: Change to enum
+  String mentorStatus;
 }
