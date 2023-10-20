295d1c74969431a4a6ef48761f750b7a85a23ff8
Maksim Tisheninov <41760206+MaxLost@users.noreply.github.com>
2023-10-20 22:01:35 +0300

    Update of data models (#8)
    * User and Event data models updated to ready-to-work state
    * Added UserDTO - minimized version of User data model mainly for future api

--- src/main/java/edu/spbu/datacontrol/models/User.java
+++ src/main/java/edu/spbu/datacontrol/models/User.java
@@ -4,8 +4,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
+import java.util.Date;
+import java.util.List;
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
 
 @Entity
 @Data
@@ -16,6 +21,39 @@ public class User {
   @GeneratedValue(strategy = GenerationType.AUTO)
   long id;
 
+  @CreatedDate
+  Date invitedAt;
+
+  boolean isActive;
+
   String name;
 
+  Date dob;
+
+  String email;
+
+  String phoneNumber;
+
+  // TODO: Maybe change to ID if supervisor is in system
+  String supervisorName;
+
+  @OneToOne
+  User teamLead;
+
+  @OneToMany
+  List<User> productOwnersIds;
+
+  // TODO: Maybe change to group ID
+  String project;
+
+  String department;
+
+  // TODO: Change to enum
+  String grade;
+
+  // TODO: Change to enum
+  String role;
+
+  // TODO: Change to enum
+  String mentorStatus;
 }
