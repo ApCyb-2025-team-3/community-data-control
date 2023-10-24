596543b75eff729102c6d082dbff8e8aee5e2d1a
Maksim Tisheninov <41760206+MaxLost@users.noreply.github.com>
2023-10-25 00:15:38 +0300

    Update EventType.java

--- src/main/java/edu/spbu/datacontrol/models/enums/EventType.java
+++ src/main/java/edu/spbu/datacontrol/models/enums/EventType.java
@@ -4,9 +4,9 @@ import lombok.Getter;
 
 @Getter
 public enum EventType {
-    ADD_USER("AddingUser"),
-    DISMISS_USER("DismissalUser"),
-    CHANGE_PROJECT("ChangeUserProject"),
+    ADD_USER("AddUser"),
+    DISMISS_USER("DismissUser"),
+    CHANGE_PROJECT("ChangeProject"),
     CHANGE_USER_DATA("ChangeUserData"),
     UPDATE_USER_GRADE("UpdateUserGrade");
 
