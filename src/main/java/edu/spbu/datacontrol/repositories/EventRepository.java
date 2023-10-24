a884d2d8ddb7ed50136f33a59d08b201b7540a29
Maksim Tisheninov <41760206+MaxLost@users.noreply.github.com>
2023-10-25 00:34:15 +0300

    Update and rename EventLogRepository.java to EventRepository.java

--- src/main/java/edu/spbu/datacontrol/repositories/EventRepository.java
+++ src/main/java/edu/spbu/datacontrol/repositories/EventRepository.java
@@ -5,7 +5,7 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface EventLogRepository extends CrudRepository<Event, Long> {
+public interface EventRepository extends CrudRepository<Event, Long> {
     Event getEventById(Long eventId);
 
     List<Event> getEventsByUserId(Long userId);
