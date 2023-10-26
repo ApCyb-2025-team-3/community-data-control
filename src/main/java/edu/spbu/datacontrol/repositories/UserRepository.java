ad05da9ef20203bd31dc581563feb45ba52cfd43
BingoBy <yangibaevnn@gmail.com>
2023-10-27 02:21:29 +0300

    Added repository for users

---
+++ src/main/java/edu/spbu/datacontrol/repositories/UserRepository.java
@@ -0,0 +1,19 @@
+package edu.spbu.datacontrol.repositories;
+
+import edu.spbu.datacontrol.models.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+@Repository
+public interface UserRepository extends CrudRepository<User, Long>{
+    User getUserById(Long userId);
+
+    User getUserByEmail(String email);
+
+    User getUserByPhoneNumber(String phoneNumber);
+
+    List<User> getUsersByName(String name);
+
+    List<User> findAllByOrderByName();
+}
