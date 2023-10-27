16590185365bab9155b8b79515563d8be291a138
Maksim Tisheninov <41760206+MaxLost@users.noreply.github.com>
2023-10-27 12:20:25 +0300

    Added role for non-members of community

--- src/main/java/edu/spbu/datacontrol/models/enums/Role.java
+++ src/main/java/edu/spbu/datacontrol/models/enums/Role.java
@@ -4,11 +4,12 @@ import lombok.Getter;
 
 @Getter
 public enum Role {
-    TEAM_MEMBER("TeamMember"),
+    MEMBER("Member"),
     DATA_ENGINEER("DataEngineer"),
     DEVELOPER("Developer"),
     TEAM_LEAD("TeamLead"),
     PRODUCT_OWNER("ProductOwner");
+    NON_MEMBER("NonMember");
 
 
     private final String description;
