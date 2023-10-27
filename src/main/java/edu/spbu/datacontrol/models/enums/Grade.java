db4c51ba38a70f36f94a0ddf03983b41dc19fde5
Maksim Tisheninov <41760206+MaxLost@users.noreply.github.com>
2023-10-27 12:18:33 +0300

    Added unspecified grade for non-members

--- src/main/java/edu/spbu/datacontrol/models/enums/Grade.java
+++ src/main/java/edu/spbu/datacontrol/models/enums/Grade.java
@@ -8,6 +8,7 @@ public enum Grade {
     MIDDLE("Middle"),
     SENIOR("Senior"),
     TEAM_LEAD("TeamLead");
+    UNSPECIFIED("Unspecified"); // Special grade for non-members such as administrative supervisors etc.
 
     private final String description;
 
