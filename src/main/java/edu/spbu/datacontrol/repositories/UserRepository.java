bc3f37fc53a6509f05b04e2f96273d1df55318b4
Maksim Tisheninov <tisheninov.maxim@gmail.com>
2023-10-30 17:09:26 +0300

    Added some tests for UserRepository

--- src/main/java/edu/spbu/datacontrol/repositories/UserRepository.java
+++ src/main/java/edu/spbu/datacontrol/repositories/UserRepository.java
@@ -3,9 +3,7 @@ package edu.spbu.datacontrol.repositories;
 import edu.spbu.datacontrol.models.User;
 import edu.spbu.datacontrol.models.enums.Role;
 import java.util.UUID;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
