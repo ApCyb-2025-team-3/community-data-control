1c4427da864cb72b9fcf1e9f7ca72a9e7670b7c5
AnSaladim <sharepin.assassin@gmail.com>
2023-11-07 20:29:05 +0300

    Some fixes(?)

--- src/main/java/edu/spbu/datacontrol/controllers/GroupController.java
+++ src/main/java/edu/spbu/datacontrol/controllers/GroupController.java
@@ -1,40 +1,41 @@
 package edu.spbu.datacontrol.controllers;
 
+import edu.spbu.datacontrol.models.Event;
 import edu.spbu.datacontrol.models.Group;
 import edu.spbu.datacontrol.models.User;
+import edu.spbu.datacontrol.models.enums.EventType;
 import edu.spbu.datacontrol.models.enums.GroupType;
+import edu.spbu.datacontrol.repositories.EventRepository;
 import edu.spbu.datacontrol.repositories.GroupRepository;
-import java.util.List;
-import edu.spbu.datacontrol.repositories.UserRepository;
 import org.springframework.http.HttpStatusCode;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 @RestController
 @RequestMapping("/api/group")
 public class GroupController {
     private GroupRepository groupRepository;
+    private EventRepository eventLog;
 
-    public GroupController(GroupRepository groupRepository) {
+    public GroupController(GroupRepository groupRepository, EventRepository eventRepository) {
+
         this.groupRepository = groupRepository;
+        this.eventLog = eventRepository;
     }
 
     @PostMapping("/create")
-    public String createGroup(GroupType type, String name, String description, User teamLead) {
+    public String createGroup(@RequestParam GroupType type, @RequestParam String name, @RequestParam String description, @RequestParam User teamLead) {
 
         Group newGroup = new Group(type, name, description, teamLead);
-        this.assignTeamLead(newGroup, teamLead);
         try {
-            this.assignTeamLead(newGroup, teamLead);
+            assignTeamLead(newGroup, teamLead);
         } catch (IllegalArgumentException e) {
             throw new ResponseStatusException(HttpStatusCode.valueOf(409), e.getMessage());
         }
 
         newGroup = groupRepository.save(newGroup);
+        Event groupCreation = new Event(newGroup.getId(), EventType.CREATE_GROUP, "");
+        eventLog.save(groupCreation);
 
         return "Group successfully created.";
     }
