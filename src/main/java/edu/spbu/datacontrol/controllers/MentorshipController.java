532f9e7d268e862eaad025a2d49c643cab19510c
Homray <st094140@student.spbu.ru>
2023-11-09 00:07:45 +0300

    Added a MentorshipController and a MentorshipRepository for it

---
+++ src/main/java/edu/spbu/datacontrol/controllers/MentorshipController.java
@@ -0,0 +1,38 @@
+package edu.spbu.datacontrol.controllers;
+
+import edu.spbu.datacontrol.models.Mentorship;
+import edu.spbu.datacontrol.models.User;
+import edu.spbu.datacontrol.models.enums.MentorshipStatus;
+import edu.spbu.datacontrol.repositories.MentorshipRepository;
+import java.util.Date;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/mentorships")
+public class MentorshipController {
+
+  private final MentorshipRepository mentorshipRepository;
+
+  public MentorshipController(MentorshipRepository mentorshipRepository) {
+    this.mentorshipRepository = mentorshipRepository;
+  }
+
+  @PostMapping("/add")
+  public ResponseEntity<String> addMentorship(@RequestBody User mentor, @RequestBody User mentee,
+      @RequestBody Date disbandmentDate) {
+    if (mentor.getMentorStatus() == MentorshipStatus.MENTOR && mentee.getMentorStatus() == MentorshipStatus.MENTEE) {
+      mentorshipRepository.save(new Mentorship(mentor, mentee, disbandmentDate));
+      return new ResponseEntity<>("Mentorship is added.", HttpStatus.OK);
+    } else {
+      return new ResponseEntity<>("The mentor or mentee is incorrectly specified.",
+          HttpStatusCode.valueOf(409));
+    }
+
+  }
+}
---
+++ src/main/java/edu/spbu/datacontrol/repositories/MentorshipRepository.java
@@ -0,0 +1,25 @@
+package edu.spbu.datacontrol.repositories;
+
+import edu.spbu.datacontrol.models.Mentorship;
+import edu.spbu.datacontrol.models.User;
+import java.util.Date;
+
+import java.util.UUID;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface MentorshipRepository extends CrudRepository<Mentorship, UUID> {
+  Mentorship getMentorshipById(UUID id);
+
+  Mentorship getMentorshipByMentor(User mentor);
+
+  Mentorship getMentorshipByMentee(User mentee);
+
+  List<Mentorship> getMentorshipsByCreationDate(Date creationDate);
+
+  List<Mentorship> getMentorshipsByDisbandmentDate(Date disbandmentDate);
+}
