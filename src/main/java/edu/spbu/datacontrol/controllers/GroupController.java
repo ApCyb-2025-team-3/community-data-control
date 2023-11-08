1810ffb216703b23557f6ba223e024f1d1dea053
Nomlsbad <iliayurasov@yandex.ru>
2023-11-09 02:16:33 +0300

    Prefix GroupController

--- src/main/java/edu/spbu/datacontrol/controllers/GroupController.java
+++ src/main/java/edu/spbu/datacontrol/controllers/GroupController.java
@@ -2,9 +2,12 @@ package edu.spbu.datacontrol.controllers;
 
 import edu.spbu.datacontrol.models.Group;
 import edu.spbu.datacontrol.models.User;
+import edu.spbu.datacontrol.models.UserDTO;
+import edu.spbu.datacontrol.models.enums.EnumUtils;
 import edu.spbu.datacontrol.models.enums.GroupType;
 import edu.spbu.datacontrol.repositories.GroupRepository;
-import org.springframework.http.HttpStatusCode;
+import edu.spbu.datacontrol.repositories.UserRepository;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -13,30 +16,36 @@ import org.springframework.web.bind.annotation.*;
 public class GroupController {
     private final GroupRepository groupRepository;
 
-    public GroupController(GroupRepository groupRepository) {
+    private final UserRepository userRepository;
+
+    public GroupController(GroupRepository groupRepository, UserRepository userRepository) {
         this.groupRepository = groupRepository;
+        this.userRepository = userRepository;
     }
 
     @PostMapping("/create")
     public ResponseEntity<String> createGroup(@RequestParam String name, @RequestParam String type,
-                                             @RequestParam String description, @RequestBody User teamLead) {
+                                              @RequestParam String description, @RequestBody UserDTO teamLeadDTO) {
         try {
-            GroupType groupType = GroupType.valueOf(type);
+            GroupType groupType = EnumUtils.fromString(GroupType.class, type);
             Group newGroup = new Group(name, groupType, description);
 
+            User teamLead = userRepository.getUserById(teamLeadDTO.getId());
             assignTeamLead(newGroup, teamLead);
+
             groupRepository.save(newGroup);
 
-            return new ResponseEntity<>("Group successfully created.", HttpStatusCode.valueOf(200));
         } catch (IllegalArgumentException e) {
             return new ResponseEntity<>(
                 "Probably, it isn't possible to assign a given user as a team leader",
-                HttpStatusCode.valueOf(404)
+                HttpStatus.BAD_REQUEST
             );
         }
+
+        return new ResponseEntity<>("Group successfully created.", HttpStatus.CREATED);
     }
 
-    public void assignTeamLead(Group group, User teamLead) throws IllegalArgumentException {
+    private void assignTeamLead(Group group, User teamLead) throws IllegalArgumentException {
         // for this we need to realize applyUser method
     }
 }
