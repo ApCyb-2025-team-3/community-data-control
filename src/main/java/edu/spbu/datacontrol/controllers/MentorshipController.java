02fb9a9fe61a9f6612aa1d5dcb99156299a63346
Chernovuk <cherpash20@gmail.com>
2023-11-11 01:17:19 +0300

    Refactored MentorshipController

--- src/main/java/edu/spbu/datacontrol/controllers/MentorshipController.java
+++ src/main/java/edu/spbu/datacontrol/controllers/MentorshipController.java
@@ -12,6 +12,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.UUID;
+
 @RestController
 @RequestMapping("/api/mentorship")
 public class MentorshipController {
@@ -26,10 +28,10 @@ public class MentorshipController {
     @PostMapping("/becomeMentee")
     public ResponseEntity<String> becomeMentee(@RequestBody UserDTO userDTO){
         try {
-            changeMentorshipStatus(userDTO, MentorshipStatus.MENTEE);
+            changeMentorshipStatus(userDTO.getId(), MentorshipStatus.MENTEE);
         }
         catch (IllegalArgumentException exception) {
-            return new ResponseEntity<>("This user is in mentorship pair already!", HttpStatusCode.valueOf(409));
+            return new ResponseEntity<>(exception.getMessage(), HttpStatusCode.valueOf(409));
 
         }
         return new ResponseEntity<>("The user has become mentee now", HttpStatusCode.valueOf(200));
@@ -38,22 +40,26 @@ public class MentorshipController {
     @PostMapping("/becomeMentor")
     public ResponseEntity<String> becomeMentor(@RequestBody UserDTO userDTO) {
         try {
-            changeMentorshipStatus(userDTO, MentorshipStatus.MENTOR);
+            changeMentorshipStatus(userDTO.getId(), MentorshipStatus.MENTOR);
         }
         catch (IllegalArgumentException exception) {
-            return new ResponseEntity<>("This user is in mentorship pair already!", HttpStatusCode.valueOf(409));
+            return new ResponseEntity<>(exception.getMessage(), HttpStatusCode.valueOf(409));
 
         }
         return new ResponseEntity<>("The user has become mentor now", HttpStatusCode.valueOf(200));
     }
 
-    private void changeMentorshipStatus(UserDTO userDTO, MentorshipStatus mentorStatus) {
-        if (mentorshipRepository.countMentorshipByMenteeOrMentor(userDTO.getId()) > 0) {
-            throw new IllegalArgumentException("IllegalArgumentException");
+    private void changeMentorshipStatus(UUID userId, MentorshipStatus mentorStatus) throws IllegalArgumentException {
+        if (isInMentorship(userId)) {
+            throw new IllegalArgumentException("This user is in mentorship pair already!");
         }
-        User user = userRepository.getUserById(userDTO.getId());
+        User user = userRepository.getUserById(userId);
         user.setMentorStatus(mentorStatus);
         userRepository.save(user);
+    }
+
+    private boolean isInMentorship(UUID userId) {
+        return mentorshipRepository.countMentorshipByMenteeOrMentor(userId) > 0;
     }
 
 }
