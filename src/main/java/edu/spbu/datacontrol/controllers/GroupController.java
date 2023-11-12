7ee3abe23a66cadcf940f209a35050b219d800c6
Vladislav Sharepin <120653824+AnSaladim@users.noreply.github.com>
2023-11-12 18:58:34 +0300

    Created disbandGroup (#41)

--- src/main/java/edu/spbu/datacontrol/controllers/GroupController.java
+++ src/main/java/edu/spbu/datacontrol/controllers/GroupController.java
@@ -7,8 +7,12 @@ import edu.spbu.datacontrol.models.UserDTO;
 import edu.spbu.datacontrol.repositories.GroupRepository;
 import edu.spbu.datacontrol.repositories.UserRepository;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/api/group")
@@ -43,7 +47,35 @@ public class GroupController {
         return new ResponseEntity<>("Group successfully created.", HttpStatus.CREATED);
     }
 
+    @PatchMapping ("/disband")
+    public ResponseEntity<String> disbandGroup(@RequestParam UUID groupId,
+                                               @RequestParam String disbandmentReason) {
+
+        Group disbandedGroup = groupRepository.findById(groupId).orElse(null);
+
+        if (disbandedGroup != null) {
+
+            Date disbandmentDate = new Date();
+            disbandedGroup.setDisbandmentDate(disbandmentDate);
+            disbandedGroup.setDisbandmentReason(disbandmentReason);
+            disbandedGroup.setActive(false);
+            disbandedGroup.setTeamLead(null);
+            dismissGroupMembers(disbandedGroup);
+            groupRepository.save(disbandedGroup);
+            return new ResponseEntity<>("Group was successfully disbanded",
+                    HttpStatusCode.valueOf(200));
+
+        }
+        return new ResponseEntity<>("This group doesn't found", HttpStatusCode.valueOf(404));
+
+    }
+
     private void assignTeamLead(Group group, User teamLead) {
+        // for this we need to realize applyUser method
+        throw new UnsupportedOperationException("Method isn't implemented.");
+    }
+
+    private void dismissGroupMembers(Group group) {
         // for this we need to realize applyUser method
         throw new UnsupportedOperationException("Method isn't implemented.");
     }
