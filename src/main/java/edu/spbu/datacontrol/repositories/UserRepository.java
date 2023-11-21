035df7853ce3c45438921d34de63e6523ed9de23
Homray <st094140@student.spbu.ru>
2023-11-21 16:25:47 +0300

    Methods "getFreeMentors" and "getFreeMentees" has been changed шin class "UserRepository";

--- src/main/java/edu/spbu/datacontrol/repositories/UserRepository.java
+++ src/main/java/edu/spbu/datacontrol/repositories/UserRepository.java
@@ -49,11 +49,11 @@ public interface UserRepository extends CrudRepository<User, UUID> {
 
     List<User> findByNameContaining(String partialName);
 
-    @Query("SELECT u FROM User u WHERE u.mentorStatus = 'mentor' " +
+    @Query("SELECT u FROM User u WHERE u.mentorStatus = 'MENTOR' " +
             "AND NOT EXISTS (SELECT m FROM Mentorship m WHERE m.mentor.id = u.id)")
     List<User> getFreeMentors();
 
-    @Query("SELECT u FROM User u WHERE u.mentorStatus = 'mentee' " +
+    @Query("SELECT u FROM User u WHERE u.mentorStatus = 'MENTEE' " +
             "AND NOT EXISTS (SELECT m FROM Mentorship m WHERE m.mentee.id = u.id)")
     List<User> getFreeMentees();
 }
