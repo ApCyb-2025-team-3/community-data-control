e0ff7f65e9468e3f4f08ffbd9dc6c18ac4454ca2
Nomlsbad <iliayurasov@yandex.ru>
2023-11-27 15:14:24 +0300

    GroupType fix

--- src/main/java/edu/spbu/datacontrol/models/enums/GroupType.java
+++ src/main/java/edu/spbu/datacontrol/models/enums/GroupType.java
@@ -7,9 +7,14 @@ public enum GroupType {
     WORKING_TEAM("Work team"),
     INTEREST_GROUP("Interest group");
 
-    private final String nameType;
+    private final String description;
 
-    GroupType(String nameType) {
-        this.nameType = nameType;
+    GroupType(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return this.getDescription();
     }
 }
